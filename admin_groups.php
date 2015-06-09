c777e143cc9196023827ff149eb249aeef481a46
adaur <adaur.underground@gmail.com>
2015-06-09 23:49:58 +0200

    PS-2

--- admin_groups.php
+++ admin_groups.php
@@ -15,8 +15,9 @@ require PUN_ROOT.'include/common.php';
 require PUN_ROOT.'include/common_admin.php';
 
 
-if ($pun_user['g_id'] != PUN_ADMIN)
+if ($pun_user['g_id'] != PUN_ADMIN) {
 	message($lang_common['No permission'], false, '403 Forbidden');
+}
 
 // Load the admin_censoring.php language file
 require PUN_ROOT.'lang/'.$admin_language.'/admin_groups.php';
