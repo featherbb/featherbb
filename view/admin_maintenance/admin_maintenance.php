e662143e0e9fbefde41d43247d5980da44e7f0a0
adaur <adaur.underground@gmail.com>
2015-06-10 15:29:22 +0200

    Separate HTML and PHP in admin_maintenance.php

---
+++ view/admin_maintenance/admin_maintenance.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Copyright (C) 2015 FeatherBB
+ * based on code by (C) 2008-2012 FluxBB
+ * and Rickard Andersson (C) 2002-2008 PunBB
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
+ 
+// Make sure no one attempts to run this script "directly"
+if (!defined('PUN'))
+	exit;
+?>
+
+	<div class="blockform">
+		<h2><span><?php echo $lang_admin_maintenance['Maintenance head'] ?></span></h2>
+		<div class="box">
+			<form method="get" action="admin_maintenance.php">
+				<div class="inform">
+					<input type="hidden" name="action" value="rebuild" />
+					<fieldset>
+						<legend><?php echo $lang_admin_maintenance['Rebuild index subhead'] ?></legend>
+						<div class="infldset">
+							<p><?php printf($lang_admin_maintenance['Rebuild index info'], '<a href="admin_options.php#maintenance">'.$lang_admin_common['Maintenance mode'].'</a>') ?></p>
+							<table class="aligntop">
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Posts per cycle label'] ?></th>
+									<td>
+										<input type="text" name="i_per_page" size="7" maxlength="7" value="300" tabindex="1" />
+										<span><?php echo $lang_admin_maintenance['Posts per cycle help'] ?></span>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Starting post label'] ?></th>
+									<td>
+										<input type="text" name="i_start_at" size="7" maxlength="7" value="<?php echo(isset($first_id)) ? $first_id : 0 ?>" tabindex="2" />
+										<span><?php echo $lang_admin_maintenance['Starting post help'] ?></span>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Empty index label'] ?></th>
+									<td class="inputadmin">
+										<label><input type="checkbox" name="i_empty_index" value="1" tabindex="3" checked="checked" />&#160;&#160;<?php echo $lang_admin_maintenance['Empty index help'] ?></label>
+									</td>
+								</tr>
+							</table>
+							<p class="topspace"><?php echo $lang_admin_maintenance['Rebuild completed info'] ?></p>
+							<div class="fsetsubmit"><input type="submit" name="rebuild_index" value="<?php echo $lang_admin_maintenance['Rebuild index'] ?>" tabindex="4" /></div>
+						</div>
+					</fieldset>
+				</div>
+			</form>
+
+			<form method="post" action="admin_maintenance.php" onsubmit="return process_form(this)">
+				<div class="inform">
+					<input type="hidden" name="action" value="prune" />
+					<fieldset>
+						<legend><?php echo $lang_admin_maintenance['Prune subhead'] ?></legend>
+						<div class="infldset">
+							<table class="aligntop">
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Days old label'] ?></th>
+									<td>
+										<input type="text" name="req_prune_days" size="3" maxlength="3" tabindex="5" />
+										<span><?php echo $lang_admin_maintenance['Days old help'] ?></span>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Prune sticky label'] ?></th>
+									<td>
+										<label class="conl"><input type="radio" name="prune_sticky" value="1" tabindex="6" checked="checked" />&#160;<strong><?php echo $lang_admin_common['Yes'] ?></strong></label>
+										<label class="conl"><input type="radio" name="prune_sticky" value="0" />&#160;<strong><?php echo $lang_admin_common['No'] ?></strong></label>
+										<span class="clearb"><?php echo $lang_admin_maintenance['Prune sticky help'] ?></span>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row"><?php echo $lang_admin_maintenance['Prune from label'] ?></th>
+									<td>
+										<select name="prune_from" tabindex="7">
+											<option value="all"><?php echo $lang_admin_maintenance['All forums'] ?></option>
+												<?php get_categories(); ?>
+											</optgroup>
+										</select>
+										<span><?php echo $lang_admin_maintenance['Prune from help'] ?></span>
+									</td>
+								</tr>
+							</table>
+							<p class="topspace"><?php printf($lang_admin_maintenance['Prune info'], '<a href="admin_options.php#maintenance">'.$lang_admin_common['Maintenance mode'].'</a>') ?></p>
+							<div class="fsetsubmit"><input type="submit" name="prune" value="<?php echo $lang_admin_common['Prune'] ?>" tabindex="8" /></div>
+						</div>
+					</fieldset>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="clearer"></div>
+</div>
---
+++ view/admin_maintenance/rebuild.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright (C) 2015 FeatherBB
+ * based on code by (C) 2008-2012 FluxBB
+ * and Rickard Andersson (C) 2002-2008 PunBB
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
+ 
+// Make sure no one attempts to run this script "directly"
+if (!defined('PUN'))
+	exit;
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo generate_page_title($page_title) ?></title>
+<style type="text/css">
+body {
+	font: 12px Verdana, Arial, Helvetica, sans-serif;
+	color: #333333;
+	background-color: #FFFFFF
+}
+
+h1 {
+	font-size: 16px;
+	font-weight: normal;
+}
+</style>
+</head>
+<body>
+
+<h1><?php echo $lang_admin_maintenance['Rebuilding index info'] ?></h1>
+<hr />
