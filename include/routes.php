5dc3093a8f82e555d66e3f7d344c096179954ee4
adaur <adaur.underground@gmail.com>
2015-06-22 21:13:00 +0200

    Better routing

--- include/routes.php
+++ include/routes.php
@@ -12,14 +12,10 @@
 $feather->get('/', '\controller\Index:display');
 
 // Viewforum
-$feather->get('/forum/:id/:name/page/:page(/)', '\controller\Viewforum:display')->conditions(array('id' => '[0-9]+', 'page' => '[0-9]+'));
-$feather->get('/forum/:id/:name(/)', '\controller\Viewforum:display')->conditions(array('id' => '[0-9]+'));
-$feather->get('/forum/:id(/)', '\controller\Viewforum:display')->conditions(array('id' => '[0-9]+'));
+$feather->get('/forum/:id(/:name)(/page/:page)(/)', '\controller\Viewforum:display')->conditions(array('id' => '[0-9]+', 'page' => '[0-9]+'));
 
 // Viewtopic
-$feather->get('/topic/:id/:name/page/:page(/)', '\controller\Viewtopic:display')->conditions(array('id' => '[0-9]+', 'page' => '[0-9]+'));
-$feather->get('/topic/:id/:name(/)', '\controller\Viewtopic:display')->conditions(array('id' => '[0-9]+'));
-$feather->get('/topic/:id(/)', '\controller\Viewtopic:display')->conditions(array('id' => '[0-9]+'));
+$feather->get('/topic/:id(/:name)(/page/:page)(/)', '\controller\Viewtopic:display')->conditions(array('id' => '[0-9]+', 'page' => '[0-9]+'));
 $feather->get('/topic/:id/action/:action(/)', '\controller\Viewtopic:action')->conditions(array('id' => '[0-9]+'));
 $feather->get('/post/:pid(/)', '\controller\Viewtopic:viewpost')->conditions(array('pid' => '[0-9]+'));
 
