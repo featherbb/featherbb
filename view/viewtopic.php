cd09a76f54a2432f2bb358e54c388742fbf6624d
adaur <adaur.underground@gmail.com>
2015-07-12 15:41:50 +0200

    Remove useless <strong> tag

--- view/viewtopic.php
+++ view/viewtopic.php
@@ -18,7 +18,7 @@ if (!defined('FEATHER')) {
 	<div class="inbox crumbsplus">
 		<ul class="crumbs">
 			<li><a href="<?php echo get_base_url() ?>/"><?php echo $lang_common['Index'] ?></a></li>
-			<li><span>»&#160;</span><strong><a href="<?php echo get_link('forum/'.$cur_topic['forum_id'].'/'.$url_forum.'/') ?>"><?php echo feather_escape($cur_topic['forum_name']) ?></a></strong></li>
+			<li><span>»&#160;</span><a href="<?php echo get_link('forum/'.$cur_topic['forum_id'].'/'.$url_forum.'/') ?>"><?php echo feather_escape($cur_topic['forum_name']) ?></a></li>
 			<li><span>»&#160;</span><strong><a href="<?php echo get_link('topic/'.$id.'/'.$url_topic.'/') ?>"><?php echo feather_escape($cur_topic['subject']) ?></a></strong></li>
 		</ul>
 		<div class="pagepost">
@@ -108,7 +108,7 @@ foreach ($post_data as $post) {
 		</div>
 		<ul class="crumbs">
 			<li><a href="<?php echo get_base_url() ?>/"><?php echo $lang_common['Index'] ?></a></li>
-			<li><span>»&#160;</span><strong><a href="<?php echo get_link('forum/'.$cur_topic['forum_id'].'/'.$url_forum.'/') ?>"><?php echo feather_escape($cur_topic['forum_name']) ?></a></strong></li>
+			<li><span>»&#160;</span><a href="<?php echo get_link('forum/'.$cur_topic['forum_id'].'/'.$url_forum.'/') ?>"><?php echo feather_escape($cur_topic['forum_name']) ?></a></li>
 			<li><span>»&#160;</span><strong><a href="<?php echo get_link('topic/'.$id.'/'.$url_topic.'/') ?>"><?php echo feather_escape($cur_topic['subject']) ?></a></strong></li>
 		</ul>
 <?php echo $subscraction ?>
