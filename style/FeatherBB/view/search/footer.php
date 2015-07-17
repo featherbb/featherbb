d4d0ab2bf7deb46ae69b04814b4b16d2851d89e1
adaur <adaur.underground@gmail.com>
2015-07-17 22:04:06 +0200

    Fix Index link

--- style/FeatherBB/view/search/footer.php
+++ style/FeatherBB/view/search/footer.php
@@ -30,7 +30,7 @@ endif;
 			<p class="pagelink"><?php echo $search['paging_links'] ?></p>
 		</div>
 		<ul class="crumbs">
-			<li><a href="<?php get_base_url() ?>"><?php echo $lang_common['Index'] ?></a></li>
+			<li><a href="<?php echo get_base_url() ?>"><?php echo $lang_common['Index'] ?></a></li>
 			<li><span>»&#160;</span><a href="<?php echo get_link('search/') ?>"><?php echo $search['crumbs_text']['show_as'] ?></a></li>
 			<li><span>»&#160;</span><strong><?php echo $search['crumbs_text']['search_type'] ?></strong></li>
 		</ul>
