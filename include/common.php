902104765754764c8ac513e3661276fd4f2fd131
adaur <adaur.underground@gmail.com>
2015-08-06 11:42:41 +0200

    Fix the Block prefetch request feature

--- include/common.php
+++ include/common.php
@@ -18,19 +18,6 @@ define('FORUM_DB_REVISION', 21);
 define('FORUM_SI_REVISION', 2);
 define('FORUM_PARSER_REVISION', 2);
 
-// Block prefetch requests
-if (isset($_SERVER['HTTP_X_MOZ']) && $_SERVER['HTTP_X_MOZ'] == 'prefetch') {
-    header('HTTP/1.1 403 Prefetching Forbidden');
-
-    // Send no-cache headers
-    header('Expires: Thu, 21 Jul 1977 07:30:00 GMT'); // When yours truly first set eyes on this world! :)
-    header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-    header('Cache-Control: post-check=0, pre-check=0', false);
-    header('Pragma: no-cache'); // For HTTP/1.0 compatibility
-
-    exit;
-}
-
 // Attempt to load the configuration file config.php
 if (file_exists(FEATHER_ROOT.'include/config.php')) {
     require FEATHER_ROOT.'include/config.php';
