efd06b02ee7ea19e20e1e7393a63b4740a616f19
beaver <bladerunner.vs@gmail.com>
2015-09-07 15:08:05 +0200

    Restore message.php view file

---
+++ featherbb/View/message.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Copyright (C) 2015 FeatherBB
+ * based on code by (C) 2008-2015 FluxBB
+ * and Rickard Andersson (C) 2002-2008 PunBB
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
+
+// Make sure no one attempts to run this script "directly"
+if (!defined('FEATHER')) {
+    exit;
+}
+
+?>
+<div id="msg" class="block error">
+	<h2><span><?= __('Error') ?></span></h2>
+	<div class="box">
+		<div class="inbox">
+			<p><?php echo $msg ?></p>
+<?php if (!$no_back_link): ?>			<p><a href="javascript: history.go(-1)"><?php _e('Go back') ?></a></p>
+<?php endif;
+    ?>		</div>
+	</div>
+</div>
