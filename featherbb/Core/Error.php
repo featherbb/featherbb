b14ab2f0904cbf00b531b2fb08fa3495e0f1b6e5
capkokoon <cap.kokoon@gmail.com>
2015-09-12 18:12:52 +0200

    Error handling, again !

--- featherbb/Core/Error.php
+++ featherbb/Core/Error.php
@@ -17,38 +17,10 @@ class Error extends \Exception
     {
         parent::__construct($message, $code);
         $this->backlink = (bool) $backlink;
-        $this->feather = \Slim\Slim::getInstance();
-        $this->display($message, $code);
     }
 
     public function hasBacklink()
     {
         return $this->backlink;
-    }
-
-    private function display($message, $code)
-    {
-        $error = array(
-            'code' => $code,
-            'message' => $message,
-            'back' => true,
-        );
-
-        // Hide internal mechanism
-        /*if (!in_array(get_class($e), array('FeatherBB\Core\Error'))) {
-            $error['message'] = 'There was an internal error'; // TODO : translation
-        }
-
-        if (method_exists($e, 'hasBacklink')) {
-            $error['back'] = $e->hasBacklink();
-        }*/
-        $this->feather->response->setStatus($code);
-        $this->feather->response->setBody(''); // Reset buffer
-        $this->feather->template->setPageInfo(array(
-            'title' => array(\FeatherBB\Core\Utils::escape($this->feather->forum_settings['o_board_title']), __('Error')),
-            'msg'    =>    $error['message'],
-            'backlink'    => $error['back'],
-        ))->addTemplate('error.php')->display();
-        $this->feather->stop();
     }
 }
