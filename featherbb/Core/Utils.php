6f05922e3f15cbe426cc196a813c7014a378e6d6
adaur <adaur.underground@gmail.com>
2016-02-19 17:27:03 +0100

    Proper getIp function

--- featherbb/Core/Utils.php
+++ featherbb/Core/Utils.php
@@ -326,7 +326,17 @@ class Utils
     //
     public static function getIp()
     {
-        // TODO: IP proxy
-        return Request::getServerParams()['REMOTE_ADDR'];
+        $client  = Request::getServerParams()['HTTP_CLIENT_IP'];
+        $forward = Request::getServerParams()['HTTP_X_FORWARDED_FOR'];
+        $remote  = Request::getServerParams()['REMOTE_ADDR'];
+
+        if (filter_var($client, FILTER_VALIDATE_IP)) {
+            return $client;
+        }
+        elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
+            return $forward;
+        }
+
+        return $remote;
     }
 }
