18dcd30cf3b24739962cbeedbdc4b6b547523114
adaur <adaur.underground@gmail.com>
2016-02-20 13:45:54 +0100

    Add toArray method

--- featherbb/Core/Interfaces/ForumSettings.php
+++ featherbb/Core/Interfaces/ForumSettings.php
@@ -8,6 +8,11 @@ class ForumSettings extends SlimSugar
 		return static::$slim->getContainer()['forum_settings'][$key];
 	}
 
+	public static function toArray()
+	{
+		return static::$slim->getContainer()['forum_settings'];
+	}
+
 	public static function set($key, $value)
 	{
 		return static::$slim->getContainer()['forum_settings'][$key] = $value;
