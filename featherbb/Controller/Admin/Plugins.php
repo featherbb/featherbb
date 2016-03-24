d961402d07e352d303bdf45d64c5c10144b5012e
beaver-dev <bladerunner.vs@gmail.com>
2016-03-24 20:29:53 +0100

    Add translations in admin plugins panel

--- featherbb/Controller/Admin/Plugins.php
+++ featherbb/Controller/Admin/Plugins.php
@@ -67,39 +67,39 @@ class Plugins
     {
         Container::get('hooks')->fire('controller.admin.plugins.activate');
 
-        if (!$args['name']) {
+        if (!$args['name'] || !is_dir(ForumEnv::get('FEATHER_ROOT').'plugins'.DIRECTORY_SEPARATOR.$args['name'])) {
             throw new Error(__('Bad request'), 400);
         }
 
         $this->model->activate($args['name']);
         // Plugin has been activated, confirm and redirect
-        return Router::redirect(Router::pathFor('adminPlugins'), 'Plugin activated!');
+        return Router::redirect(Router::pathFor('adminPlugins'), sprintf(__('Plugin activated'), $args['name']));
     }
 
     public function deactivate($req, $res, $args)
     {
         Container::get('hooks')->fire('controller.admin.plugins.deactivate');
 
-        if (!$args['name']) {
+        if (!$args['name'] || !is_dir(ForumEnv::get('FEATHER_ROOT').'plugins'.DIRECTORY_SEPARATOR.$args['name'])) {
             throw new Error(__('Bad request'), 400);
         }
 
         $this->model->deactivate($args['name']);
-        // // Plugin has been deactivated, confirm and redirect
-        return Router::redirect(Router::pathFor('adminPlugins'), array('warning', 'Plugin deactivated!'));
+        // Plugin has been deactivated, confirm and redirect
+        return Router::redirect(Router::pathFor('adminPlugins'), array('warning', sprintf(__('Plugin deactivated'), $args['name'])));
     }
 
     public function uninstall($req, $res, $args)
     {
         Container::get('hooks')->fire('controller.admin.plugins.uninstall');
 
-        if (!$args['name']) {
+        if (!$args['name'] || !is_dir(ForumEnv::get('FEATHER_ROOT').'plugins'.DIRECTORY_SEPARATOR.$args['name'])) {
             throw new Error(__('Bad request'), 400);
         }
 
         $this->model->uninstall($args['name']);
-        // Plugin has been deactivated, confirm and redirect
-        return Router::redirect(Router::pathFor('adminPlugins'), array('warning', 'Plugin uninstalled!'));
+        // Plugin has been uninstalled, confirm and redirect
+        return Router::redirect(Router::pathFor('adminPlugins'), array('warning', sprintf(__('Plugin uninstalled'), $args['name'])));
     }
 
     /**
