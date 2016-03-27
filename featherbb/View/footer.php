a85f80897d869f122d00c2c897865bed330a4e4d
beaver-dev <bladerunner.vs@gmail.com>
2016-03-27 19:46:39 +0200

    Move footer out of <section>
    Prettify ident, add style for footer .blocktable

--- featherbb/View/footer.php
+++ featherbb/View/footer.php
@@ -17,20 +17,21 @@ if (!isset($feather)) {
 Container::get('hooks')->fire('view.footer.start');
 ?>
         </div>
-
+    </section>
+    <footer class="container">
         <div id="brdfooter" class="block">
             <h2><span><?php _e('Board footer') ?></span></h2>
             <div class="box">
 <?php
 
 if (isset($active_page) && ($active_page == 'Forum' || $active_page == 'Topic') && User::get()->is_admmod) {
-    echo "\t\t".'<div id="modcontrols" class="inbox">'."\n";
+    echo "\t\t\t\t".'<div id="modcontrols" class="inbox">'."\n";
 
     if ($active_page == 'Forum') {
-        echo "\t\t\t".'<dl>'."\n";
-        echo "\t\t\t\t".'<dt><strong>'.__('Mod controls').'</strong></dt>'."\n";
-        echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moderateForum', ['fid' => $fid, 'page' => $page_number]).'">'.__('Moderate forum').'</a></span></dd>'."\n";
-        echo "\t\t\t".'</dl>'."\n";
+        echo "\t\t\t\t\t".'<dl>'."\n";
+        echo "\t\t\t\t\t\t".'<dt><strong>'.__('Mod controls').'</strong></dt>'."\n";
+        echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moderateForum', ['fid' => $fid, 'page' => $page_number]).'">'.__('Moderate forum').'</a></span></dd>'."\n";
+        echo "\t\t\t\t\t".'</dl>'."\n";
     } elseif ($active_page == 'Topic') {
         if (isset($pid)) {
             $parameter = $pid;
@@ -39,42 +40,38 @@ if (isset($active_page) && ($active_page == 'Forum' || $active_page == 'Topic') 
         } else {
             $parameter = '';
         }
-
-
-        echo "\t\t\t".'<dl>'."\n";
-        echo "\t\t\t\t".'<dt><strong>'.__('Mod controls').'</strong></dt>'."\n";
-
-        echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moderateTopic', ['id' => $tid, 'fid' => $fid, 'page' => $page_number]).'">'.__('Moderate topic').'</a></span></dd>'."\n";
-        echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moveTopic', ['id' => $tid, 'fid' => $fid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Move topic').'</a></span></dd>'."\n";
+        echo "\t\t\t\t\t".'<dl>'."\n";
+        echo "\t\t\t\t\t\t".'<dt><strong>'.__('Mod controls').'</strong></dt>'."\n";
+        echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moderateTopic', ['id' => $tid, 'fid' => $fid, 'page' => $page_number]).'">'.__('Moderate topic').'</a></span></dd>'."\n";
+        echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('moveTopic', ['id' => $tid, 'fid' => $fid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Move topic').'</a></span></dd>'."\n";
 
         if ($cur_topic['closed'] == '1') {
-            echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('openTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Open topic').'</a></span></dd>'."\n";
+            echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('openTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Open topic').'</a></span></dd>'."\n";
         } else {
-            echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('closeTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Close topic').'</a></span></dd>'."\n";
+            echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('closeTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Close topic').'</a></span></dd>'."\n";
         }
 
         if ($cur_topic['sticky'] == '1') {
-            echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('unstickTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Unstick topic').'</a></span></dd>'."\n";
+            echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('unstickTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Unstick topic').'</a></span></dd>'."\n";
         } else {
-            echo "\t\t\t\t".'<dd><span><a href="'.Router::pathFor('stickTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Stick topic').'</a></span></dd>'."\n";
+            echo "\t\t\t\t\t\t".'<dd><span><a href="'.Router::pathFor('stickTopic', ['id' => $tid, 'name' => Url::url_friendly($cur_topic['subject'])]).'">'.__('Stick topic').'</a></span></dd>'."\n";
         }
 
-        echo "\t\t\t".'</dl>'."\n";
+        echo "\t\t\t\t\t".'</dl>'."\n";
     }
 
-    Container::get('hooks')->fire('view.footer.mod.actions');
-
-    echo "\t\t\t".'<div class="clearer"></div>'."\n\t\t".'</div>'."\n";
-}
-
-?>
+    Container::get('hooks')->fire('view.footer.mod.actions'); ?>
+                    <div class="clearer"></div>
+                </div>
+<?php } ?>
                 <div id="brdfooternav" class="inbox">
 <?php
 // Display the "Jump to" drop list
 if (ForumSettings::get('o_quickjump') == '1' && !empty($quickjump)) { ?>
                     <div class="conl">
                         <form id="qjump" method="get" action="">
-                            <div><label><span><?php _e('Jump to') ?><br /></span></label>
+                            <div>
+                                <label><span><?php _e('Jump to') ?><br /></span></label>
                                 <select name="id" onchange="window.location=(this.options[this.selectedIndex].value)">
 <?php
                 foreach ($quickjump[(int) User::get()->g_id] as $cat_id => $cat_data) {
@@ -93,49 +90,50 @@ if (ForumSettings::get('o_quickjump') == '1' && !empty($quickjump)) { ?>
                     <div class="conr">
                         <p id="poweredby"><?php printf(__('Powered by'), '<a href="http://featherbb.org/">FeatherBB</a>'.((ForumSettings::get('o_show_version') == '1') ? ' '.ForumSettings::get('o_cur_version') : '')) ?></p>
                     </div>
-                <div class="clearer"></div>
+                    <div class="clearer"></div>
+                </div>
             </div>
         </div>
-    </div>
 <?php
 
 // Display debug info (if enabled/defined)
 if (!empty($exec_info)) { ?>
-    <p id="debugtime">[ <?= sprintf(__('Querytime'), round($exec_info['exec_time'], 6), $exec_info['nb_queries']).' - '.sprintf(__('Memory usage'), $exec_info['mem_usage']).' '.sprintf(__('Peak usage'), $exec_info['mem_peak_usage'])?> ]</p>
+        <p id="debugtime">[ <?= sprintf(__('Querytime'), round($exec_info['exec_time'], 6), $exec_info['nb_queries']).' - '.sprintf(__('Memory usage'), $exec_info['mem_usage']).' '.sprintf(__('Peak usage'), $exec_info['mem_peak_usage'])?> ]</p>
 <?php }
 if (!empty($queries_info)) { ?>
-    <div id="debug" class="blocktable">
-        <h2><span><?php _e('Debug table') ?></span></h2>
-        <div class="box">
+        <div id="debug" class="blocktable">
+            <h2><span><?php _e('Debug table') ?></span></h2>
+            <div class="box">
                 <div class="inbox">
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="tcl" scope="col"><?php _e('Query times') ?></th>
-                            <th class="tcr" scope="col"><?php _e('Query') ?></th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="tcl" scope="col"><?php _e('Query times') ?></th>
+                                <th class="tcr" scope="col"><?php _e('Query') ?></th>
+                            </tr>
+                        </thead>
+                        <tbody>
 <?php foreach ($queries_info['raw'] as $time => $sql) {
     echo "\t\t\t\t\t\t".'<tr>'."\n";
     echo "\t\t\t\t\t\t\t".'<td class="tcl">'.Utils::escape(round($time, 8)).'</td>'."\n";
     echo "\t\t\t\t\t\t\t".'<td class="tcr">'.Utils::escape($sql).'</td>'."\n";
     echo "\t\t\t\t\t\t".'</tr>'."\n";
 } ?>
-                        <tr>
-                            <td class="tcl" colspan="2"><?= sprintf(__('Total query time'), round($queries_info['total_time'], 7)).' s' ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+                            <tr>
+                                <td class="tcl" colspan="2"><?= sprintf(__('Total query time'), round($queries_info['total_time'], 7)).' s' ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
 <?php } ?>
-</section>
+    </footer>
 </body>
 <!-- JS -->
 <script>
     var baseUrl = '<?= Utils::escape(Url::base()); ?>',
+        bodyId = 'pun<?= $active_page; ?>',
         phpVars = <?= isset($jsVars) ? json_encode($jsVars) : json_encode(array()); ?>;
 </script>
 <?php
