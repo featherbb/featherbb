8c2b6f5aff706ff8e3fda693f3c198885589f945
beaver-dev <bladerunner.vs@gmail.com>
2016-04-17 18:44:33 +0200

    Add isAdmin and isAdminMod method in User interface

--- featherbb/Core/Interfaces/User.php
+++ featherbb/Core/Interfaces/User.php
@@ -7,7 +7,7 @@ class User extends \Statical\BaseProxy
 {
     /**
      * Load a user infos
-     * @param  integer $id The id of the user. If null (default), will return currently logged user
+     * @param  int    $id The id of the user. If null (default), will return currently logged user
      * @return object     User infos taken from database
      */
     public static function get($id = null)
@@ -24,7 +24,7 @@ class User extends \Statical\BaseProxy
     /**
      * Get a user preference value
      * @param  string $pref The name of preference to get
-     * @param  integer $id  Optionnal user id. If not provided, will return pref for currently logged user
+     * @param  int     $id  Optionnal user id. If not provided, will return pref for currently logged user
      * @return string       Value of the pref returned by Core/Preferences class
      */
     public static function getPref(string $pref = null, int $id = null)
@@ -36,12 +36,33 @@ class User extends \Statical\BaseProxy
     /**
      * Check if the given user has the required permissions
      * @param  string $permission  The name of the action to check
-     * @param  integer $id         Optionnal user id
+     * @param  int     $id         Optionnal user id
      * @return boolval             True if user is allowed to do this
      */
     public static function can(string $permission = null, int $id = null)
     {
         $user = self::get($id);
         return Container::get('perms')->can($user, $permission);
+    }
+
+    /**
+     * Check if user is in admin group
+     * @param  int     $id Optionnal user id
+     * @return boolean     Is user in admin group ?
+     */
+    public static function isAdmin(int $id = null)
+    {
+        return self::get($id)->g_id == ForumEnv::get('FEATHER_ADMIN');
+    }
+
+    /**
+     * Check if user is admin or modo
+     * @param  int     $id Optionnal user id
+     * @return boolean     Is user in admin/mod group ?
+     */
+    public static function isAdminMod(int $id = null)
+    {
+        $user = self::get($id);
+        return $user->g_id == ForumEnv::get('FEATHER_ADMIN') || Container::get('perms')->can($user, 'mod.is_mod');
     }
 }
