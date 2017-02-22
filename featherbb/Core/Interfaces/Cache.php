878cf7cba1a30426c3d68856391f13ceceb00fde
adaur <adaur.underground@gmail.com>
2017-02-22 16:33:46 +0100

    Add Cache interface

---
+++ featherbb/Core/Interfaces/Cache.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrien
+ * Date: 22/02/2017
+ * Time: 12:16
+ */
+
+namespace Core\Interfaces;
+
+
+class Cache
+{
+
+}
---
+++ style/themes/FeatherBB/boostrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrien
+ * Date: 21/02/2017
+ * Time: 16:31
+ */
